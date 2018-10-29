571def8df41c5a92f77f87132c6555253249e80c
benjamindeuson@gmail.com <benjamindeuson@gmail.com>
2018-10-28 21:13:03 -0700

    Functionalize the bates numbering logic

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from os.path import join
 from marisol import Marisol
 
 
-number_machine = Marisol(prefix='BATES_NUMBER_', fill=6, start=1)  # Begin numbering with BATES_NUMBER_000001
-number_machine.append(join('input_files', 'fb_target.pdf'))
-number_machine.append(join('input_files', 'fb_target.pdf'))
-result = number_machine.save()  # Export numbered PDFs, named for the bates number.
-# Log the names of the files processed and their statuses (ex. ('file_one': True))
-print(f'Result: {result}')
+def apply_numbering(prefix='BATES_NUMBER_', backfill_zeroes=6, start_no=1):
+    number_machine = Marisol(prefix=prefix, fill=backfill_zeroes, start=start_no)
+    number_machine.append(join('input_files', 'fb_target.pdf'))
+    number_machine.append(join('input_files', 'fb_target.pdf'))
+    # Export numbered PDFs, named for their first bates number.
+    result = number_machine.save()
+    # Log the names of the files processed and their statuses (ex. ('file_one': True))
+    print(f'Result: {result}')
