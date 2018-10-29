84742ae585e4ca6572a9633b7699079b70f2d468
Obleskar <obleskar@gmail.com>
2018-10-28 21:22:01 -0700

    Docstring: Add a docstring to apply_numbering()

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from marisol import Marisol
 
 
 def apply_numbering(prefix='BATES_NUMBER_', backfill_zeroes=6, start_no=1):
+    """Apply incrementing "bates stamps" to each provided file.
+
+    Arguments:
+        prefix (str): Standard prefix for each page's "bates stamp."
+        backfill_zeroes (int): Maximum number of zeros that must appear after the prefix. Defaults to 6.
+            Backfilled zeroes are consumed by the count number as it increments.
+        start_no (int): Number to start the count on. Defaults to 1.
+
+    Returns:
+        list: Tuples with the filename and a corresponding boolean success indicator.
+    """
     number_machine = Marisol(prefix=prefix, fill=backfill_zeroes, start=start_no)
     number_machine.append(join('input_files', 'fb_target.pdf'))
     number_machine.append(join('input_files', 'fb_target.pdf'))
