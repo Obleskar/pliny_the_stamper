7d40b5d44fe5618f53684ef9f67105fc69af1f57
benjamindeuson@gmail.com <benjamindeuson@gmail.com>
2018-10-28 21:23:48 -0700

    Return the numbering result in apply_numbering

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def apply_numbering(prefix='BATES_NUMBER_', backfill_zeroes=6, start_no=1):
     number_machine.append(join('input_files', 'fb_target.pdf'))
     number_machine.append(join('input_files', 'fb_target.pdf'))
     # Export numbered PDFs, named for their first bates number.
-    result = number_machine.save()
+    status = number_machine.save()
     # Log the names of the files processed and their statuses (ex. ('file_one': True))
-    print(f'Result: {result}')
+    print(f'Result: {status}')
+    return status
